7c1dce0034f3a98cf99b09f42c57eb79a66e03cf
宅男丶辉辉 <11588978+aa61@user.noreply.gitee.com>
2024-06-25 14:12:11 +0800

    导航栏

---
+++ src/views/user/userRole.vue
@@ -0,0 +1,99 @@
+<template>
+  <div ></div>
+</template>
+<script>
+import { createOrUpdate, deleteUserById, queryUser } from '@/api/user'
+import { getBrandEnum, getRoleEnum } from '@/api/enum'
+
+export default {
+  name: 'User',
+  data() {
+    return {
+      dialogStatus: 'create',
+      dialogFormVisible: false,
+      page: 1,
+      size: 10,
+      list: null,
+      total: null,
+      listLoading: true,
+      listQuery: {
+        page: 1,
+        size: 10
+      },
+      titleMap: {
+        create: '新增用户',
+        update: '编辑用户'
+      },
+      sortable: null,
+      temp: {},
+      brands: [],
+      roles: []
+    }
+  },
+  created() {
+    this.getBrands()
+    this.getRoles()
+    this.getList(1)
+  },
+  methods: {
+    async getList(page) {
+      this.listQuery.page = page
+      this.listQuery.size = this.size
+      this.listLoading = true
+      const { data, total } = await queryUser(this.listQuery)
+      this.list = data
+      this.total = total
+      this.listLoading = false
+    },
+
+    beforeCreate() {
+      this.temp = {}
+      this.dialogStatus = 'create'
+      this.dialogFormVisible = true
+    },
+
+    beforeUpdate(row) {
+      this.temp = JSON.parse(JSON.stringify(row))
+      this.dialogStatus = 'update'
+      this.dialogFormVisible = true
+    },
+
+    async getBrands() {
+      const { data } = await getBrandEnum()
+      this.brands = data
+    },
+
+    async getRoles() {
+      const { data } = await getRoleEnum()
+      this.roles = data
+    },
+
+    deleteUser(row) {
+      deleteUserById(row.id).then((res) => {
+        if (res.data) {
+          this.$message.success('删除成功')
+          this.getList(this.page)
+        } else {
+          this.$message.error('删除失败')
+        }
+      })
+    },
+
+    createOrUpdateUser(status, data) {
+      if (status === 'create') {
+        data.id = null
+      }
+      createOrUpdate(data).then((res) => {
+        if (res.data) {
+          this.$message.success('操作成功')
+          this.getList(this.page)
+        }
+        this.dialogFormVisible = false
+      })
+    }
+  }
+}
+</script>
+
+  <style>
+  </style>
